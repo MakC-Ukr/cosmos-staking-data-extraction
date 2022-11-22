91191d5d18028785f697a452a92ee6c4ecb186af
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-11-22 22:43:40 +0530

    .

--- helpers.py
+++ helpers.py
@@ -24,6 +24,17 @@ CMC_headers = {
     'X-CMC_PRO_API_KEY': '',
     'Accept': 'application/json',
 }
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 def get_inflation():    
     response = requests.get(RPC_URL+'/cosmos/mint/v1beta1/inflation', headers=headers).json()
